0779aa4d8e8c0e6f91b908406380c158be8a61b2
Emerson Gaudencio <emerson.fender@gmail.com>
2020-01-07 12:36:05 +0000

    Beta version 1.0.4

--- ansible/run_mariadb_install.sh
+++ ansible/run_mariadb_install.sh
@@ -6,12 +6,11 @@ export ANSIBLE_CONFIG=$SCRIPT_PATH/ansible.cfg
 
 cd $SCRIPT_PATH
 
-VAR_MARIADB_HOST="dbtest02"
-VAR_SSH_USER="master"
-VAR_MARIADB_VERSION="103"
+VAR_HOST="$1"
+VAR_MARIADB_VERSION="$2"
 
 ### Ping host ####
-ansible -i $SCRIPT_PATH/hosts -m ping $VAR_MARIADB_HOST -u $VAR_SSH_USER -o
+ansible -i $SCRIPT_PATH/hosts -m ping $VAR_HOST -v
 
 ### MariaDB install ####
-ansible-playbook -v -i $SCRIPT_PATH/hosts -e "{mariadb_version: '$VAR_MARIADB_VERSION'}" $SCRIPT_PATH/playbook/mariadb_install.yml -l $VAR_MARIADB_HOST  -u $VAR_SSH_USER
+ansible-playbook -v -i $SCRIPT_PATH/hosts -e "{mariadb_version: '$VAR_MARIADB_VERSION'}" $SCRIPT_PATH/playbook/mariadb_install.yml -l $VAR_HOST
