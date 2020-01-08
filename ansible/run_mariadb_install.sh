3c3aabbe5925b01b5ccbd3259716eb575aecb237
Emerson Gaudencio <emerson.fender@gmail.com>
2020-01-08 23:22:11 +0000

    Release Candidate 1.0.1

--- ansible/run_mariadb_install.sh
+++ ansible/run_mariadb_install.sh
@@ -9,8 +9,14 @@ cd $SCRIPT_PATH
 VAR_HOST="$1"
 VAR_MARIADB_VERSION="$2"
 
-### Ping host ####
-ansible -i $SCRIPT_PATH/hosts -m ping $VAR_HOST -v
 
-### MariaDB install ####
-ansible-playbook -v -i $SCRIPT_PATH/hosts -e "{mariadb_version: '$VAR_MARIADB_VERSION'}" $SCRIPT_PATH/playbook/mariadb_install.yml -l $VAR_HOST
+if [ "$VAR_MARIADB_VERSION" -gt 0 -a "$VAR_HOST" != "" ]; then
+  ### Ping host ####
+  ansible -i $SCRIPT_PATH/hosts -m ping $VAR_HOST -v
+
+  ### MariaDB install ####
+  ansible-playbook -v -i $SCRIPT_PATH/hosts -e "{mariadb_version: '$VAR_MARIADB_VERSION'}" $SCRIPT_PATH/playbook/mariadb_install.yml -l $VAR_HOST
+else
+  echo "Sorry, this script must have 2 parameters to run. So first of all you have to fill up the first parameter with the ansible hostname and the second parameter MariaDB version, please have a look at README file for futher information!"
+  exit 1
+fi
