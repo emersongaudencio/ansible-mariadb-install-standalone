a04bc67383f4809df9af49f78e78ddb3910eb7aa
Emerson Gaudencio <emerson.fender@gmail.com>
2020-09-09 01:38:19 +0100

    adding security enhancements.

--- ansible/playbook/files/mariadb_install/mariadb_install_config.sh
+++ ansible/playbook/files/mariadb_install/mariadb_install_config.sh
@@ -351,6 +351,7 @@ mysql -e "GRANT PROCESS ON *.* TO '$MYSQLCHK_USER_NAME'@'%' IDENTIFIED BY '$MYSQ
 mysql -e "DELETE FROM mysql.user WHERE User='';";
 mysql -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
 mysql -e "UPDATE mysql.user SET Password=PASSWORD('$hash') WHERE User='root';"
+mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'127.0.0.1' IDENTIFIED BY '$hash' WITH GRANT OPTION;"
 mysql -e "flush privileges;"
 
 ### REMOVE TMP FILES on /tmp #####
